7614c12b7b3123a5a05ad660529cb433b895b51b
mumugisha <prudent.nkubito@gmail.com>
2024-12-07 21:23:04 +0200

    Queuing System in JS

--- utils/redis.js
+++ utils/redis.js
@@ -1,12 +1,57 @@
-import redisClient from './utils/redis.js';
+import redis from 'redis';
+import { promisify } from 'util';
 
-(async () => {
-  console.log(redisClient.isAlive());
-  console.log(await redisClient.get('myKey'));
-  await redisClient.set('myKey', 12, 5);
-  console.log(await redisClient.get('myKey'));
+class RedisClient {
+  constructor() {
+    this.client = redis.createClient();
+    this.getAsync = promisify(this.client.get).bind(this.client);
 
-  setTimeout(async () => {
-    console.log(await redisClient.get('myKey'));
-  }, 1000 * 10);
-})();
+    this.client.on('connect', () => {
+      console.log('Redis client connected to the server');
+    });
+
+    this.client.on('error', (err) => {
+      console.log('Redis client not connected to the server:', err);
+    });
+  }
+
+  /**
+   * Method that checks if the connection is alive
+   * @return {boolean} true if the connection is alive or false if not
+   */
+  isAlive() {
+    return this.client.connected;
+  }
+
+  /**
+   * Method to get the value of a key
+   * @param {string} key The key to retrieve
+   * @return {string|null} The value of the key, or null if not found
+   */
+  async get(key) {
+    const value = await this.getAsync(key);
+    return value;
+  }
+
+  /**
+   * Method to set a key-value pair with expiration
+   * @param {string} key The key to set
+   * @param {string} value The value to set
+   * @param {number} duration The expiration time in seconds
+   */
+  set(key, value, duration) {
+    this.client.setex(key, duration, value);
+  }
+
+  /**
+   * Method to delete a key
+   * @param {string} key The key to delete
+   * @return {void}
+   */
+  delete(key) {
+    this.client.del(key);
+  }
+}
+
+const redisClient = new RedisClient();
+export default redisClient;
