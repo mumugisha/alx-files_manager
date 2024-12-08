508bb27ce03f65259a8a2fd31dd72db40fab8c48
mumugisha <prudent.nkubito@gmail.com>
2024-12-08 19:18:44 +0200

    Files manager

---
+++ controllers/UsersController.js
@@ -0,0 +1,60 @@
+import { ObjectId } from 'mongodb';
+import sha1 from 'sha1';
+import dbClient from '../utils/db';
+import UserUtils from '../utils/user';
+
+class UserController {
+  static async postNew(req, res) {
+    const { email, password } = req.body;
+
+    if (!email) {
+      return res.status(400).json({ error: 'Missing email' });
+    }
+
+    if (!password) {
+      return res.status(400).json({ error: 'Missing password' });
+    }
+
+    const existingEmail = await dbClient.usersCollection.findOne({ email });
+    if (existingEmail) {
+      return res.status(400).json({ error: 'Already exists' });
+    }
+
+    const encryptedPassword = sha1(password);
+
+    try {
+      const newUser = { email, password: encryptedPassword };
+      const result = await dbClient.usersCollection.insertOne(newUser);
+
+      return res.status(201).json({ id: result.insertedId, email });
+    } catch (err) {
+      console.error('Error creating new user:', err.message);
+      return res.status(500).json({ error: 'Internal server error' });
+    }
+  }
+
+  static async getMe(req, res) {
+    try {
+      const { userId } = await UserUtils.getUserIdAndKey(req);
+
+      const user = await UserUtils.getUser({
+        _id: ObjectId(userId),
+      });
+
+      if (!user) {
+        return res.status(401).send({ error: 'Unauthorized' });
+      }
+
+      const processedUser = { id: user._id, ...user };
+      delete processedUser._id;
+      delete processedUser.password;
+
+      return res.status(200).send(processedUser);
+    } catch (err) {
+      console.error('Error retrieving user information:', err.message);
+      return res.status(500).json({ error: 'Internal server error' });
+    }
+  }
+}
+
+export default UserController;
