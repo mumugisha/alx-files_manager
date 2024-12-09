a0d4c00c9440412769df12a09ae56bb0e5f6df6a
mumugisha <prudent.nkubito@gmail.com>
2024-12-10 01:29:59 +0200

    Files manager

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
-import express from 'express';
-import controllingRoutes from './routes/index.js`';
-
+const controllingRoutes = require('./routes/index.js');
+const express = require('express');
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -8,9 +7,8 @@ app.use(express.json());
 
 controllingRoutes(app);
 
-
 app.listen(5000, () => {
   console.log('Server is running on port 5000');
 });
 
-export default app;
+module.exports = app;
