98974e2e35dbd4267794641fe2fdad41ed7ca5d3
mumugisha <prudent.nkubito@gmail.com>
2024-12-12 02:10:45 +0200

    File management

--- tests/utils/db.test.js
+++ tests/utils/db.test.js
@@ -1,31 +1,36 @@
+/* eslint-disable import/no-named-as-default */
 import dbClient from '../../utils/db';
 import { expect } from 'chai';
 
 describe('+ DBClient utility', () => {
-  before(function (done) {
+  before((done) => {
     this.timeout(10000);
     Promise.all([dbClient.usersCollection(), dbClient.filesCollection()])
-      .then(([usersCollection, filesCollection]) => {
-        return Promise.all([
+      .then(([usersCollection, filesCollection]) => 
+        Promise.all([
           usersCollection.deleteMany({}),
-          filesCollection.deleteMany({})
-        ]);
-      })
+          filesCollection.deleteMany({}),
+        ])
+      )
       .then(() => done())
-      .catch((err) => done(err));
+      .catch((error) => done(error));
   });
 
   it('+ Client is alive', () => {
+    expect.assertions(1);
     expect(dbClient.isAlive()).to.equal(true);
   });
 
   it('+ nbUsers returns the correct value', async () => {
-    const nbUsers = await dbClient.nbUsers();
-    expect(nbUsers).to.equal(0);
+    expect.assertions(1);
+    const users = await dbClient.nbUsers();
+    expect(users).to.equal(0);
   });
 
   it('+ nbFiles returns the correct value', async () => {
-    const nbFiles = await dbClient.nbFiles();
-    expect(nbFiles).to.equal(0);
+    expect.assertions(1);
+    const files = await dbClient.nbFiles();
+    expect(files).to.equal(0);
   });
 });
+
