2c3a5cc727b5eb094b6f1f20e9dabf57ccf47230
mumugisha <prudent.nkubito@gmail.com>
2024-12-12 02:13:18 +0200

    File management

--- server.js
+++ server.js
@@ -1,14 +1,27 @@
+require('dotenv').config();
 const controllingRoutes = require('./routes/index.js');
 const express = require('express');
+const helmet = require('helmet');
 const app = express();
 const port = process.env.PORT || 5000;
 
+app.use(helmet());
 app.use(express.json());
 
 controllingRoutes(app);
 
-app.listen(5000, () => {
-  console.log('Server is running on port 5000');
+app.listen(port, (err) => {
+  if (err) {
+    console.error('Failed to start the server:', err);
+  } else {
+    console.log(`Server is running on port ${port}`);
+  }
+});
+
+// Error logging middleware
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send('Something went wrong!');
 });
 
 module.exports = app;
